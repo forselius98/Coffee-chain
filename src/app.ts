abb5a15d8022b654d498a7b5c3f9a01a6163e868
forselius98 <elfor_98@outlook.com>
2026-08-17 13:44:21 +0200

    ändring i JSON

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import blockchainRoutes from './models/routes/blockchainRoutes.ts';
+import blockchainRoutes from './models/routes/blockchainRoutes.js';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api', blockchainRoutes);
