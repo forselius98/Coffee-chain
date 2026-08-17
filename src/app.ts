4adb6b60ae2c4dd49d78262988a3ad19aa14fb51
forselius98 <elfor_98@outlook.com>
2026-08-17 14:26:17 +0200

    uppdaterad port

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import blockchainRoutes from './models/routes/blockchainRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api', blockchainRoutes);
+    
 
 export default app;
